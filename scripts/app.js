bd8e87f1de480c8d0da0223b16ec9ce6fe4ca1ce
pwhitdog <paul.whitmer@gmail.com>
2016-03-28 12:45:44 -0700

    Initial timer and angurlar created.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,31 @@
-alert('Test of alert');
+var app = angular.module('mobApp', []);
+
+app.controller('mobController', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.minutes = 15;
+    $scope.seconds = 10;
+    $scope.secondsDisplay = '';
+    
+    countDownTimeOut();
+
+    function countDown() {
+        if ($scope.seconds === 0) {
+            $scope.minutes -= 1;
+            $scope.seconds = 59;
+        } else {
+            $scope.seconds -= 1;
+        }
+        if($scope.seconds < 10) {
+            $scope.secondsDisplay = '0' + $scope.seconds;
+        }
+        else {
+            $scope.secondsDisplay = $scope.seconds;
+        }
+    }
+
+    function countDownTimeOut() {
+        countDown();
+        if($scope.minutes >= 0 && $scope.seconds >= 0) {            
+            $timeout(countDownTimeOut, 1000);
+        }
+    }
+}]);
